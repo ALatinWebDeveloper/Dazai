90889bc6c33a806cc5bda17b95c51d1095931713
Marcus <juandbeta@gmail.com>
2024-04-25 00:53:50 -0400

    added the img files and created the gallery json aswell as the data file

---
+++ scripts/gallery.js
@@ -0,0 +1,16 @@
+url = "https://github.com/ALatinWebDeveloper/Dazai/data/galery.json";
+
+async function getGallery() {
+
+    response = await fetch(url);
+    data = await response.json();
+
+    displayGallery(data);
+}
+
+const displayGallery = (data) => {
+
+    console.table(data);
+}
+
+getGallery();
