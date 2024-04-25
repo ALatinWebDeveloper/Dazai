0f9aa537252dd9982ad75829c9432127bfa10415
Marcus <juandbeta@gmail.com>
2024-04-25 01:56:13 -0400

    assemblement of 3d gallery completed and json gallery rute fixed

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,16 +1,46 @@
 const url = "https://ALatinWebDeveloper.github.io/Dazai/data/gallery.json";
+const blenderGallery = document.querySelectorAll(".Gallery-container")[0];
+const drawsGallery = document.querySelectorAll(".Gallery-container")[1];
+
 
 async function getGallery() {
 
     response = await fetch(url);
     data = await response.json();
 
-    displayGallery(data);
+    display3dGallery(data.gallery);
 }
 
-const displayGallery = (data) => {
+const display3dGallery = (data) => {
 
-    console.log(data.gallery);
+    i = 0;
+    
+    data.blender.forEach(imageData => {
+        
+        //console.log(imageData);
+
+        //Creating the elements to assemble the gallery
+
+        div = document.createElement("div");
+        img = document.createElement("img");
+
+        //Populating the elements of the gallery
+
+        div.addEventListener("onclick", abrirPopup(i));
+        div.classList.add("img-container");
+
+        img.classList.add("img-fit");
+        img.src = imageData[i];
+        img.alt = `Gallery image ${i}`;
+
+        //Assembling the 3D gallery
+
+        blenderGallery.appendChild(div);
+        div.appendChild(img);
+
+        //index counter
+        i++;
+    });
 }
 
 getGallery();
